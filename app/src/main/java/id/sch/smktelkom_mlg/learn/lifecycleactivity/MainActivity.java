07695dcd15b65d2c2d95c9bd10902513028a3fbc
yefta <yefta>
2017-03-01 11:25:25 +0700

    Initial commit Livecycle Basic

--- app/src/main/java/id/sch/smktelkom_mlg/learn/lifecycleactivity/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/lifecycleactivity/MainActivity.java
@@ -2,12 +2,52 @@ package id.sch.smktelkom_mlg.learn.lifecycleactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LivecycleTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
     }
 }
